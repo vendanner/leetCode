c1a4ce81d96b5f9e9daf755a712228d28d82ee14
jasonwin10 <jasonlyt@yeah.net>
2019-01-19 18:23:12 +0800

    Intersection of Two Arrays II

---
+++ 简单/350_两个数组的交集 II/sort.cpp
@@ -0,0 +1,48 @@
+/**
+给定两个数组，编写一个函数来计算它们的交集。
+
+示例 1:
+
+输入: nums1 = [1,2,2,1], nums2 = [2,2]
+输出: [2,2]
+示例 2:
+
+输入: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+输出: [4,9]
+说明：
+
+输出结果中每个元素出现的次数，应与元素在两个数组中出现的次数一致。
+我们可以不考虑输出结果的顺序。
+进阶:
+
+如果给定的数组已经排好序呢？你将如何优化你的算法？
+如果 nums1 的大小比 nums2 小很多，哪种方法更优？
+如果 nums2 的元素存储在磁盘上，磁盘内存是有限的，并且你不能一次加载所有的元素到内存中，你该怎么办？
+
+解法：
+	先对数组排序，然后直接数组顺序比对
+	小数据量的情况下，数组比对比map 方式快很多
+*/
+#include <algorithm> 
+
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+    	sort(nums1.begin(),nums1.end());
+    	sort(nums2.begin(),nums2.end());
+
+    	vector<int> result;
+    	for(int i=0,j=0;i<nums1.size()&&j<nums2.size();){
+    		if(nums1[i] < nums2[j]){
+    			i++;
+    		}else if(nums1[i] > nums2[j]){
+    			j++;
+    		}else{
+    			result.push_back(nums1[i]);
+    			i++;
+    			j++;
+    		}
+    	}
+    	return result;
+    }
+};
